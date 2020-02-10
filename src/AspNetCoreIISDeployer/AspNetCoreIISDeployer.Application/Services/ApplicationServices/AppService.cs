3d656270b38692178ea9f0ed6c094929520f43d7
Árva Balázs <balazs.arva@windowslive.com>
2020-02-10 22:05:11 +0100

    WIP: watching publish folders and reading configuration.

---
+++ src/AspNetCoreIISDeployer/AspNetCoreIISDeployer.Application/Services/ApplicationServices/AppService.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreIISDeployer.Application.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AspNetCoreIISDeployer.Application.Services.ApplicationServices
+{
+    public class AppService
+    {
+        /*
+        public void WatchFolder(string path)
+        {
+            var watcher = new FileSystemWatcher(path);
+
+            watcher.
+        }
+        */
+
+            /*
+        public async Task<AppListModel> GetAppsAsync(string globalConfigurationFilePath, string userOverridesFilePath)
+        {
+            var globalConfiguration = JsonConvert.DeserializeObject<List<AppModel>>(await File.ReadAllTextAsync(globalConfigurationFilePath));
+            var userOverrides = JsonConvert
+                .DeserializeObject<List<AppModel>>(await File.ReadAllTextAsync(userOverridesFilePath))
+                .ToDictionary(x => x.Id);
+
+            var userOverrides2 = JArray.Parse(await File.ReadAllTextAsync(userOverridesFilePath));
+
+            foreach (var app in userOverrides2)
+            {
+                var children = app.Children();
+
+                app.Children().Any(c => c.Path.EndsWith("/HttpPort"));
+
+            }
+
+
+            foreach (var app in globalConfiguration)
+            {
+                var app1 = 
+
+                if (userOverrides.ContainsKey(app.Id))
+                {
+                    var userOverride = userOverrides[app.Id];
+
+                }
+            }
+
+            return new AppListModel { Apps = globalConfiguration };
+        }*/
+    }
+}
