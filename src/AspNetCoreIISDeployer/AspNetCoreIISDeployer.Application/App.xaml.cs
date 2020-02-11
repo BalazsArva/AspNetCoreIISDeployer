7b103d9cf54ca1f5cd3a25d5d6d24505cb68ae78
Árva Balázs <balazs.arva@windowslive.com>
2020-02-11 19:57:40 +0100

    Dependency injection.

--- src/AspNetCoreIISDeployer/AspNetCoreIISDeployer.Application/App.xaml.cs
+++ src/AspNetCoreIISDeployer/AspNetCoreIISDeployer.Application/App.xaml.cs
@@ -1,38 +1,24 @@
-﻿using System.Windows;
-using AspNetCoreIISDeployer.Application.Configuration;
-using AspNetCoreIISDeployer.Application.Services.ApplicationServices;
-using AspNetCoreIISDeployer.Application.Services.DotNet;
-using AspNetCoreIISDeployer.Application.Services.Git;
-using AspNetCoreIISDeployer.Application.Services.IIS;
-using AspNetCoreIISDeployer.Application.ViewModels;
-using AspNetCoreIISDeployer.Application.ViewModels.Factories;
+﻿using System;
+using System.Windows;
+using AspNetCoreIISDeployer.Application.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreIISDeployer.Application
 {
     public partial class App : System.Windows.Application
     {
+        private readonly IServiceProvider serviceProvider;
+
         public App()
         {
+            serviceProvider = ServiceConfiguration.BuildServiceProvider();
         }
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            var dotNetConfig = new DotNetConfiguration();
-            var iisConfig = new IISMangementConfiguration();
-            var gitConfig = new GitConfiguration();
-
-            IGitService gitService = new GitService(gitConfig);
-            IDotNetPublishService publishService = new DotNetPublishService(dotNetConfig, gitService);
-            ISiteManagementService siteManagementService = new SiteManagementService(iisConfig);
-
-            IAppViewModelFactory appViewModelFactory = new AppViewModelFactory(publishService, siteManagementService, gitService);
-            IAppService appService = new AppService();
-
-            IAppListViewModelFactory appListViewModelFactory = new AppListViewModelFactory(appViewModelFactory, appService);
-
-            MainWindow = new MainWindow(new MainWindowViewModel(appListViewModelFactory));
+            MainWindow = serviceProvider.GetRequiredService<MainWindow>();
             MainWindow.Show();
         }
     }
